46d3835795f79dfc8f79e16125d5c19ac4a04e56
yaser samsam <yaser.alsamsam@gmail.com>
2025-04-16 03:10:22 +0300

    upload updates

--- resources/views/awtar/footerAndNav/footer.blade.php
+++ resources/views/awtar/footerAndNav/footer.blade.php
@@ -84,12 +84,12 @@
                               <!-- Facebook -->
                               <div class="icon facebook">
                                   <div class="tooltip">Facebook</div>
-                                  <span><a href="https://m.facebook.com/p/awtar__alstanate-100067601478316/"><i class="fa fa-facebook"></i></a></span>
+                                  <span><a href="https://www.facebook.com/profile.php?id=61575203096918"><i class="fa fa-facebook"></i></a></span>
                               </div>
                               <!-- Instagram -->
                               <div class="icon instagram">
                                   <div class="tooltip">Instagram</div>
-                                  <span><a href="https://www.instagram.com/awtar_alsultanate?igsh=YjdwY3J3ZTBrdXEx"><i class="fa fa-instagram"></i></a></span>
+                                  <span><a href="https://www.instagram.com/awtar_alsultanate_perfum?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i class="fa fa-instagram"></i></a></span>
                               </div>
                            <!-- Youtube -->
                             <div class="icon youtube">
@@ -111,7 +111,7 @@
                             {{-- titok --}}
                             <div class="icon tiktok">
                               <div class="tooltip">TikTok</div>
-                              <a herf="https://www.tiktok.com/@awtaralsltanate?_r=1&_d=e6ahfc65517hk1&sec_uid=MS4wLjABAAAAumHwlNzRRMofpFbUpxec0KZywbqAf_EMpzibHAxSjrEZmdmB6FIkk0ZbogRefW5T&share_author_id=7378892867537830918&sharer_language=ar&source=h5_m&u_code=d3kgcmae5gab81&ug_btm=b2001,b5836&sec_user_id=MS4wLjABAAAANSQNfUJ2-6pgCm7O4EXdgDNPdE7FALBItCQDdj7mQH0tut9ZzPainQOqq099tsjc&utm_source=whatsapp&social_share_type=5&utm_campaign=client_share&utm_medium=ios&tt_from=whatsapp&user_id=6637958970638991365&enable_checksum=1&share_link_id=36F2F7B4-4516-4B0C-A263-E05F31E7B23D&share_app_id=1233">
+                              <a herf="https://www.tiktok.com/@awtar_alsultanate_perfum?_t=ZS-8vY6iQVTtcl&_r=1">
                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fa-tiktok" viewBox="0 0 16 16">
                                    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
                                  </svg>
